969d7407db8dd1e494e8ca3f122179c4675ad42d
ldhan <ldhan0115@gmail.com>
2026-03-09 18:33:31 +0900

    chore: initialize project

---
+++ src/main/java/com/study/study_community_platform/StudyCommunityPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.study.study_community_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCommunityPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyCommunityPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/study_community_platform/StudyCommunityPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.study_community_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCommunityPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
