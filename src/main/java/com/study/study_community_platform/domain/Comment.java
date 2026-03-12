c21527b85eca1ddaa10bff49c80c86ba6f0457c2
ldhan <ldhan0115@gmail.com>
2026-03-12 12:31:56 +0900

    feat: create Comment entity

---
+++ src/main/java/com/study/study_community_platform/domain/Comment.java
@@ -0,0 +1,47 @@
+package com.study.study_community_platform.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+    // 댓글의 기본키 (PK)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    // 댓글이 작성된 게시글
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    // 댓글 작성자
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    // 댓글 내용
+    @Lob
+    private String content;
+
+    // 댓글 생성 시간
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    // 댓글 수정 시간
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    // soft delete 처리용 컬럼
+    private LocalDateTime deletedAt;
+}
