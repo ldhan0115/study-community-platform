8d5a428e9a960a0d56dd8a8f7449a76da868160e
ldhan <ldhan0115@gmail.com>
2026-03-12 11:01:54 +0900

    create Study entity

---
+++ src/main/java/com/study/study_community_platform/domain/StudyMethod.java
@@ -0,0 +1,6 @@
+package com.study.study_community_platform.domain;
+
+// 스터디 진행 방식
+public enum StudyMethod {
+    ONLINE, OFFLINE
+}
---
+++ src/main/java/com/study/study_community_platform/domain/StudyStatus.java
@@ -0,0 +1,6 @@
+package com.study.study_community_platform.domain;
+
+// 스터디 모집 상태
+public enum StudyStatus {
+    OPEN, CLOSED
+}
